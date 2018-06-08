0febee83a415b2af1c60b040d27572b9f9f8dc62
havatar11m <havatar11m@hotmail.com>
2018-06-08 14:00:50 +0200

    Actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner
+
 public class Main {
 
+    public static void main(String[] args){
+        Scanner escaner = new Scanner(System.in);
+        int filas = 5;
+        int columnas = 10;
+        char [][] parking = new char[filas][columnas];
+        int recaudacion = 0;
+        int menu = 0;
 
+    }
 }
